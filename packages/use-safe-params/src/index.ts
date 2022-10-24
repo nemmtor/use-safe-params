98b28d3c35d0caf41b658aa55ace9cefcf9090c5
nemmtor <kacper0witas@gmail.com>
2022-10-24 23:12:46 +0200

    changes

--- packages/use-safe-params/src/index.ts
+++ packages/use-safe-params/src/index.ts
@@ -9,3 +9,6 @@ export const useSafeParams = <Path extends string>() => {
   assertParamsAreValid(params);
   return params as Simplify<typeof params>;
 };
+
+// eslint-disable-next-line no-console
+console.log('test');
